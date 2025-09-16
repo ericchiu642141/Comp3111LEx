ee381b4dda06d997ef8643c4e2a368e27585ffa7
Ericchiu <ericchiu642141@gmail.com>
2025-09-16 13:56:28 +0800

    This is an initial commit for Comp3111LEx Project

--- src/main/java/Lab1/mainApp1.java
+++ src/main/java/Lab1/mainApp1.java
@@ -5,7 +5,7 @@ public class mainApp1 {
         int b = 2;
         int n = 11;
         System.out.println("Program ..Starting...");
-        System.out.println("1 + 1 = " + 2);
+        System.out.println("1 + 1 = "+2);
         System.out.println(b+" to power "+n+" = "+ myLibrary.Power(b,n));
         System.out.println(n+"! = " + myLibrary.factorial(n));
         System.out.println("Program ..Ended …");
